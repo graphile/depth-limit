e368fe071a88c2096643baf2c2409f17e4b5c155
Benjie Gillam <benjie@jemjie.com>
2024-04-15 16:26:51 +0100

    Extract common helpers from test

--- __tests__/basics.test.mjs
+++ __tests__/basics.test.mjs
@@ -1,49 +1,11 @@
 // @ts-check
 import assert from "node:assert";
 import { describe, it } from "node:test";
-import { buildSchema, validate, parse } from "graphql";
-import { maxDepth } from "../dist/index.js";
-
-const schema = buildSchema(/* GraphQL */ `
-  type Query {
-    currentUser: User
-  }
-  type User {
-    name: String
-    settings: UserSettings
-    friends: [User!]!
-    friendsConnection: UserConnection!
-  }
-  type UserSettings {
-    sendNotifications: Boolean
-  }
-  type UserConnection {
-    edges: [FriendEdge]!
-    pageInfo: PageInfo
-  }
-  type PageInfo {
-    hasNextPage: Boolean
-  }
-  type FriendEdge {
-    cursor: String!
-    node: User
-  }
-`);
-
-/**
- * @param {string} operationText
- * @param {import("../dist/index.js").Options} [options]
- */
-function doValidate(operationText, options) {
-  const document = parse(operationText);
-  const rule = maxDepth(options);
-  const errors = validate(schema, document, [rule]);
-  return errors;
-}
+import { parseAndValidate } from "./utils.mjs";
 
 describe("depth limit", () => {
   it("works", () => {
-    const errors = doValidate(
+    const errors = parseAndValidate(
       /* GraphQL */ `
         {
           currentUser {
---
+++ __tests__/utils.mjs
@@ -0,0 +1,40 @@
+// @ts-check
+import { buildSchema, parse, validate } from "graphql";
+import { maxDepth } from "../dist/index.js";
+
+export const schema = buildSchema(/* GraphQL */ `
+  type Query {
+    currentUser: User
+  }
+  type User {
+    name: String
+    settings: UserSettings
+    friends: [User!]!
+    friendsConnection: UserConnection!
+  }
+  type UserSettings {
+    sendNotifications: Boolean
+  }
+  type UserConnection {
+    edges: [FriendEdge]!
+    pageInfo: PageInfo
+  }
+  type PageInfo {
+    hasNextPage: Boolean
+  }
+  type FriendEdge {
+    cursor: String!
+    node: User
+  }
+`);
+
+/**
+ * @param {string} operationText
+ * @param {import("../dist/index.js").Options} [options]
+ */
+export function parseAndValidate(operationText, options) {
+  const document = parse(operationText);
+  const rule = maxDepth(options);
+  const errors = validate(schema, document, [rule]);
+  return errors;
+}
